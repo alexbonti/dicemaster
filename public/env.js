6c795999ddb572fbf41a3bb610d17402a654c16c
alexbonti <aledeakin@gmail.com>
2020-05-12 20:37:41 +1000

    functioning dicemaster service

--- public/env.js
+++ public/env.js
@@ -1,4 +1,30 @@
+var randomNumber=function(max){
+  return Math.floor(Math.random() * Math.floor(max));
+} 
+
+var remoteRoll=function(max){
+  $.get('/roll',{number:max},function(result){
+    console.log(result)
+    $('#spanText').text(result.rolledNumber)   
+
+  })
+
+}
+
+
 $(document).ready(function(){
   console.log('Ready')
+  $('#randomButton').click(function(){
+    $('#spanText').text(randomNumber(10))   
+  })
+  $('#d4Button').click(function(){
+    remoteRoll(4)
+  })
+  $('#d6Button').click(function(){
+    $('#spanText').text(randomNumber(6))   
+  })
+  $('#d20Button').click(function(){
+    $('#spanText').text(randomNumber(20))   
+  })
 
 })
--- server.js
+++ server.js
@@ -3,12 +3,27 @@ var express = require("express"),
 
 var port = process.env.PORT || 8080;
 
+
+/// this function generates a random number
+var randomNumber=function(max){
+  return Math.floor(Math.random() * Math.floor(max));
+} 
+
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
+
+app.get('/roll',function(request,response){
+  var numberMax=request.query.number;
+  var rolled=randomNumber(numberMax)
+  response.send({rolledNumber:rolled})
+
+})
 
 app.listen(port);
 console.log("Listening on port ", port);
